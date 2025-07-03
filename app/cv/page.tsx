155ac2edc6897d962c463b63d0209c3e17eb6118
banjaeha <myemail@example.combanjaehavana@gmail.com>
2025-07-03 19:58:42 +0900

    cv페이지 에러 수정

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,18 +1,9 @@
 'use client';
-import { useRouter, useSearchParams } from "next/navigation";
+
+import { useRouter } from "next/navigation";
 import CVPanel from "@/components/CVPanel";
 
 export default function CVPage() {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const from = searchParams.get("from");
-
-  return (
-    <CVPanel
-      onClose={() => {
-        if (from) router.replace(from);
-        else router.back();
-      }}
-    />
-  );
+  return <CVPanel onClose={() => router.back()} />;
 }
