f68d93a62455e7304f7d4bf73b85eac631b02631
banjaeha <myemail@example.combanjaehavana@gmail.com>
2025-07-05 13:46:24 +0900

    이전 주소 리다이렉트 연결

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,24 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {    eslint: {
-  ignoreDuringBuilds: true,
-},
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/playhomesweethome',
+        destination: '/diagram/modal/projects/play-home',
+        permanent: true,
+      },
+      {
+        source: '/makehomesweethome',
+        destination: '/diagram/modal/projects/make-home',
+        permanent: true,
+      },
+      // 다른 리다이렉트도 필요하면 여기 추가
+    ];
+  },
 };
 
 export default nextConfig;
